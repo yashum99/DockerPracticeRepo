38a645298c6dfe007eedb0391f3966f0549d125e
Yashwant <yashwantmishr@gmail.com>
2026-03-12 10:07:35 +0530

    commit-1

---
+++ DockerBootAppProj-01/src/main/java/com/yashu/DockerBootAppProj01Application.java
@@ -0,0 +1,16 @@
+package com.yashu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBootAppProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBootAppProj01Application.class, args);
+	
+		System.out.println("Welcome in docker tool using springboot app");
+	
+	}
+
+}
---
+++ DockerBootAppProj-01/src/test/java/com/yashu/DockerBootAppProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBootAppProj01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
